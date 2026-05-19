185546bfe9a3fbd1d8bcba2d69d9c6f16d0dba84
Xeon0X <xeon0x.gaming@gmail.com>
2026-05-19 14:50:01 +0200

    setup: uv and dbt

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # CATTE(D)RALL
+
 ELT
+
+## Setup
+
+Ce projet utilise `uv` comme gestionnaire de dépendances Python. `dbt` est une bibliothèque Python utilisée pour la transformation des données. On utilise l'adaptateur Postgres.
+
+Pour synchroniser les dépendances (e.g. installer `dbt-postgres`) :
+
+```bash
+uv sync
+```
 
 ## Useful commands
 
@@ -8,7 +19,7 @@ dbt deps
 dbt init
 ```
 
-## STEPS
+## Steps
 
 1. CSV -> /seeds
 2. Database connection details: in ~/.dbt/profiles.yml
---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            LD_LIBRARY_PATH =
+              with pkgs;
+              nixpkgs.lib.makeLibraryPath [
+                stdenv.cc.cc.lib
+                libGL
+                glib
+              ];
+            venvDir = ".venv";
+            packages =
+              with pkgs;
+              [
+                python314
+                uv
+              ]
+              ++ (with pkgs.python314Packages; [
+                pip
+                venvShellHook
+              ]);
+          };
+        }
+      );
+    };
+}
